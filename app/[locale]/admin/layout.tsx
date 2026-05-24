f300735b0c9c8c46d2e6a0f8fe82120fa352ec0c
Matias Sendra 2 <matu2982@hotmail.com>
2026-05-23 23:18:53 -0300

    fix: use scrollbar-gutter stable to reserve scrollbar space

--- app/[locale]/admin/layout.tsx
+++ app/[locale]/admin/layout.tsx
@@ -36,7 +36,7 @@ export default async function AdminLayout({
         userEmail={user.email || undefined}
         userAvatar={user.user_metadata?.avatar_url}
       />
-      <div className="flex-grow overflow-y-auto">
+      <div className="flex-grow overflow-y-auto" style={{ scrollbarGutter: 'stable' }}>
         {children}
       </div>
     </div>
